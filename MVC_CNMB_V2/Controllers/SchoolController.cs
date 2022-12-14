93dcd9a576391a2e00aad4c44bcab49973011ef0
lilbandit1979 <stephenobrien1979@Gmail.com>
2022-12-14 22:00:33 +0000

    --Get all schools working --POST not working possible issue with ID

--- MVC_CNMB_V2/Controllers/SchoolController.cs
+++ MVC_CNMB_V2/Controllers/SchoolController.cs
@@ -38,6 +38,28 @@ namespace MVC_CNMB_V2.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult create(SchoolViewModel school)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5021/api/school"); 
+
+                //HTTP POST
+                var postTask = client.PostAsJsonAsync<SchoolViewModel>("school", school);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+            return View(school);
+        }
     }
 }
 
